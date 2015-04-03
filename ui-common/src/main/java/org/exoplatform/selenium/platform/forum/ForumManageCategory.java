2854b215d107eff58e15eaa8161a41952f9e7882
OssoRomain <rosso@exoplatform.com>
2015-04-03 10:56:18 +0700

    FQA-2326:PLF 4.2/FORUM FNC High - Migrate commons and test cases (Forum/Category/Edit)

--- ui-common/src/main/java/org/exoplatform/selenium/platform/forum/ForumManageCategory.java
+++ ui-common/src/main/java/org/exoplatform/selenium/platform/forum/ForumManageCategory.java
@@ -111,7 +111,7 @@ public class ForumManageCategory extends ForumBase {
 	//-------------------Message-------------------------------------
 	public String MESSAGE_CATEGORY_NOT_EXISTE ="The category isn't existed";
 	public String MESSAGE_RESTRICTED_AUDIENCE_INVALID="The field \"Restricted Audience\" is invalid: ";
-	public String MESSAGE_CATEGORY_MODERATOR_INVALID = "not found, please enter a valid value.";
+	public String MESSAGE_CATEGORY_MODERATOR_INVALID = " not found, please enter a valid value.";
 	
 	/*------------------------------------common function---------------------------------*/
 
