7277575cbf66d3c46e4d62c1ed694c68964d57ea
ptquynh <ptquynhhn@gmail.com>
2015-09-07 10:12:59 +0700

    FQA-2640:PLF 4.3 - Write High Func/Wiki/BasicAction/Resize

---
+++ ui-testsuite/src/main/java/org/exoplatform/selenium/platform/wiki/functional/Wiki_BasicAction_Resize.java
@@ -0,0 +1,314 @@
+package org.exoplatform.selenium.platform.wiki.functional;
+
+import static org.exoplatform.selenium.TestLogger.info;
+
+import org.testng.annotations.*;
+
+
+	public class Wiki_BasicAction_Resize extends WIKI_TestConfig{
+
+	/**
+	*<li> Case ID:139457.</li>
+	*<li> Test Case Name: Show the resize pointer from Intranet.</li>
+	*<li> Pre-Condition: </li>
+	*<li> Post-Condition: </li>
+	*SHOULD TEST BY MANUAL
+	*/
+	@Test(groups="pending")
+	public  void test01_ShowTheResizePointerFromIntranet() {
+		info("Test 1: Show the resize pointer from Intranet");
+		/*Step Number: 1
+		*Step Name: Step 1: Check panel limits don't show
+		*Step Description: 
+			- Go to Intranet/Wiki
+		*Input Data: 
+			
+		*Expected Outcome: 
+			Panel limits are not displayed*/
+
+		/*Step number: 2
+		*Step Name: Step 2: Show the resize pointer
+		*Step Description: 
+			Roll over the panel limit
+		*Input Data: 
+			
+		*Expected Outcome: 
+			- The mouse pointer changes to a "resize" pointer
+			- Panels limit are displayed*/ 
+
+ 	}
+
+	/**
+	*<li> Case ID:139458.</li>
+	*<li> Test Case Name: Show resize pointer from Space.</li>
+	*<li> Pre-Condition: create a space</li>
+	*<li> Post-Condition: </li>
+	*SHOULD TEST BY MANUAL
+	*/
+	@Test(groups="pending")
+	public  void test02_ShowResizePointerFromSpace() {
+		info("Test 2: Show resize pointer from Space");
+		/*Step Number: 1
+		*Step Name: Step 1: Check panel limits don't show
+		*Step Description: 
+			- Go to Space/Wiki
+		*Input Data: 
+			
+		*Expected Outcome: 
+			Panel limits are not displayed*/
+
+		/*Step number: 2
+		*Step Name: Step 2: Show resize pointer
+		*Step Description: 
+			Roll over the panel limit
+		*Input Data: 
+			
+		*Expected Outcome: 
+			- The mouse pointer changes to a "resize" pointer
+			- Panels limit are displayed*/ 
+
+ 	}
+
+	/**
+	*<li> Case ID:139459.</li>
+	*<li> Test Case Name: Resize panel from Intranet.</li>
+	*<li> Pre-Condition: </li>
+	*<li> Post-Condition: </li>
+	*SHOULD TEST BY MANUAL
+	*/
+	@Test(groups="pending")
+	public  void test03_ResizePanelFromIntranet() {
+		info("Test 3: Resize panel from Intranet");
+		/*Step Number: 1
+		*Step Name: Step 1: Resize panel
+		*Step Description: 
+			- Go to Intranet/Wiki
+			- Roll over the panel
+			- Resize the panel
+		*Input Data: 
+			
+		*Expected Outcome: 
+			- The panel is resized with the wished size
+			- Panels limits are masked*/ 
+
+ 	}
+
+	/**
+	*<li> Case ID:139460.</li>
+	*<li> Test Case Name: Resize panel from Space.</li>
+	*<li> Pre-Condition: create space</li>
+	*<li> Post-Condition: </li>
+	*SHOULD TEST BY MANUAL
+	*/
+	@Test(groups="pending")
+	public  void test04_ResizePanelFromSpace() {
+		info("Test 4: Resize panel from Space");
+		/*Step Number: 1
+		*Step Name: Step 1: Resize panel
+		*Step Description: 
+			- Go to Space/Wiki
+			- Roll over the panel
+			- Resize the panel
+		*Input Data: 
+			
+		*Expected Outcome: 
+			- The panel is resized with the wished size
+			- Panels limits are masked*/ 
+
+ 	}
+
+	/**
+	*<li> Case ID:139471.</li>
+	*<li> Test Case Name: Width of panel across user sessions from Intranet.</li>
+	*<li> Pre-Condition: </li>
+	*<li> Post-Condition: </li>
+	*SHOULD TEST BY MANUAL
+	*/
+	@Test(groups="pending")
+	public  void test05_WidthOfPanelAcrossUserSessionsFromIntranet() {
+		info("Test 5: Width of panel across user sessions from Intranet");
+		/*Step Number: 1
+		*Step Name: - Resize the panel
+		*Step Description: 
+			- Go to Intranet/Wiki
+			- Rollover the left panel until a wished postion
+		*Input Data: 
+			
+		*Expected Outcome: 
+			- The panel is resized
+			- The resize pointer is hidden*/
+
+		/*Step number: 2
+		*Step Name: - Verify the width of panel
+		*Step Description: 
+			- Navigate in others screen (ex: go to intranet/forum)
+			- Back to Intranet/Wiki
+		*Input Data: 
+			
+		*Expected Outcome: 
+			The size of the panel is saved*/ 
+
+ 	}
+
+	/**
+	*<li> Case ID:139472.</li>
+	*<li> Test Case Name: Width of panel across user sessions from Space.</li>
+	*<li> Pre-Condition: create a space</li>
+	*<li> Post-Condition: </li>
+	*SHOULD TEST BY MANUAL
+	*/
+	@Test(groups="pending")
+	public  void test06_WidthOfPanelAcrossUserSessionsFromSpace() {
+		info("Test 6: Width of panel across user sessions from Space");
+		/*Step Number: 1
+		*Step Name: - Resize the panel
+		*Step Description: 
+			- Go to Space/Wiki
+			- Rollover the left panel until a wished postion
+		*Input Data: 
+			
+		*Expected Outcome: 
+			- The panel is resized
+			- The resize pointer is hidden*/
+
+		/*Step number: 2
+		*Step Name: - Verify the width of panel
+		*Step Description: 
+			- Navigate in others screen (ex: go to intranet/forum)
+			- Back to Intranet/Wiki
+		*Input Data: 
+			
+		*Expected Outcome: 
+			The size of the panel is saved*/ 
+
+ 	}
+
+	/**
+	*<li> Case ID:139473.</li>
+	*<li> Test Case Name: Width of panel specific by user from Intranet.</li>
+	*<li> Pre-Condition: </li>
+	*<li> Post-Condition: </li>
+	*SHOULD TEST BY MANUAL
+	*/
+	@Test(groups="pending")
+	public  void test07_WidthOfPanelSpecificByUserFromIntranet() {
+		info("Test 7: Width of panel specific by user from Intranet");
+		/*Step Number: 1
+		*Step Name: - Resize the panel
+		*Step Description: 
+			- Connect with User A
+			- Go to Intranet/Wiki
+			- Roll over the panel limits and resize
+		*Input Data: 
+			
+		*Expected Outcome: 
+			Thepanel is resized by the user A*/
+
+		/*Step number: 2
+		*Step Name: - Check the width of panel with other users
+		*Step Description: 
+			- Connect with User B
+			- Go to Intranet/Wiki
+		*Input Data: 
+			
+		*Expected Outcome: 
+			The default size of the wiki application is shown*/
+
+		/*Step number: 3
+		*Step Name: - Verify the width of panel
+		*Step Description: 
+			Re connect with the User A
+		*Input Data: 
+			
+		*Expected Outcome: 
+			The size fixed by the user A is displayed*/ 
+
+ 	}
+
+	/**
+	*<li> Case ID:139474.</li>
+	*<li> Test Case Name: Width of panel specific by user from Space.</li>
+	*<li> Pre-Condition: create a spaceUser A and B are member in this space</li>
+	*<li> Post-Condition: </li>
+	*SHOULD TEST BY MANUAL
+	*/
+	@Test(groups="pending")
+	public  void test08_WidthOfPanelSpecificByUserFromSpace() {
+		info("Test 8: Width of panel specific by user from Space");
+		/*Step Number: 1
+		*Step Name: - Resize the panel
+		*Step Description: 
+			- Connect with User A
+			- Go to Space/Wiki
+			- Roll over the panel limits and resize
+		*Input Data: 
+			
+		*Expected Outcome: 
+			Thepanel is resized by the user A*/
+
+		/*Step number: 2
+		*Step Name: - Check the width of panel with other users
+		*Step Description: 
+			- Connect with User B
+			- Go to Intranet/Wiki
+		*Input Data: 
+			
+		*Expected Outcome: 
+			The default size of the wiki application is shown*/
+
+		/*Step number: 3
+		*Step Name: - Verify the width of panel
+		*Step Description: 
+			Re connect with the User A
+		*Input Data: 
+			
+		*Expected Outcome: 
+			The size fixed by the user A is displayed*/ 
+
+ 	}
+
+	/**
+	*<li> Case ID:139477.</li>
+	*<li> Test Case Name: Action bar after a resize of the left panel from Intranet.</li>
+	*<li> Pre-Condition: </li>
+	*<li> Post-Condition: </li>
+	*SHOULD TEST BY MANUAL
+	*/
+	@Test(groups="pending")
+	public  void test09_ActionBarAfterAResizeOfTheLeftPanelFromIntranet() {
+		info("Test 9: Action bar after a resize of the left panel from Intranet");
+		/*Step Number: 1
+		*Step Name: Step 1: Check resize the left pane form Space
+		*Step Description: 
+			- Go to Space/Wiki
+			- Roll over panel limit
+			- Resize the left panel to maximum
+		*Input Data: 
+			
+		*Expected Outcome: 
+			When the size to display them is too small on only one single line, we use multiple lines.*/ 
+
+ 	}
+
+	/**
+	*<li> Case ID:139478.</li>
+	*<li> Test Case Name: Action bar after a resize of the left panel from Space.</li>
+	*<li> Pre-Condition: </li>
+	*<li> Post-Condition: </li>
+	*SHOULD TEST BY MANUAL
+	*/
+	@Test(groups="pending")
+	public  void test10_ActionBarAfterAResizeOfTheLeftPanelFromSpace() {
+		info("Test 10 Action bar after a resize of the left panel from Space");
+		/*Step Number: 1
+		*Step Name: Step 1: Check resize the left pane form Space
+		*Step Description: 
+			- Go to Space/Wiki
+			- Roll over panel limit
+			- resize the left panel to maximum
+		*Input Data: 
+			
+		*Expected Outcome: 
+			When the size to display them is too small on only one single line, we use multiple lines.*/ 
+
+ 	}}
